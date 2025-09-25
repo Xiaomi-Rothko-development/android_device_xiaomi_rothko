ed9c4797418c22c2644319ca87eadc9018a48768
Yukino1314520 <1475215798@qq.com>
2025-09-25 14:28:22 +0800

    rothko: set namespaces

--- device.mk
+++ device.mk
@@ -170,6 +170,17 @@ PRODUCT_COPY_FILES += \
 # Shipping API level
 PRODUCT_SHIPPING_API_LEVEL := 34
 
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH) \
+    hardware/google/pixel \
+    hardware/google/interfaces \
+    hardware/lineage/interfaces/power-libperfmgr \
+    hardware/mediatek \
+    hardware/mediatek/libmtkperf_client \
+    hardware/xiaomi
+
+
 # Rootdir
 PRODUCT_PACKAGES += \
     fstab.mt6989 \
