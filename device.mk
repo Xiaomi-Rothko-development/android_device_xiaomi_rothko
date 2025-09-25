62636242995534d9cd09e71c5b8c8712c311cfac
Yukino1314520 <1475215798@qq.com>
2025-09-25 14:33:48 +0800

    rothko: Build Vibrator

--- device.mk
+++ device.mk
@@ -198,6 +198,15 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vndservicemanager
 
+# Vibrator
+$(call soong_config_set, vibrator, vibratortargets, vibratoraidlV2target)
+
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.vibrator.service
+
+PRODUCT_COPY_FILES += \
+    vendor/qcom/opensource/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
+
 # Rootdir
 PRODUCT_PACKAGES += \
     fstab.mt6989 \
