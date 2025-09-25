0f2af9dd58368da5345724281d7a099a033640cb
Yukino1314520 <1475215798@qq.com>
2025-09-25 14:11:36 +0800

    rothko: Build Bluetooth audio

--- device.mk
+++ device.mk
@@ -70,6 +70,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml
 
+# Bluetooth
+PRODUCT_PACKAGES += \
+    android.hardware.bluetooth.audio-impl
+
 # Rootdir
 PRODUCT_PACKAGES += \
     fstab.mt6989 \
