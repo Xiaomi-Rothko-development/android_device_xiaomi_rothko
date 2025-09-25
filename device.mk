2fa5145ad3d467a0f28cfbc6a102d641df1cbc3b
Yukino1314520 <1475215798@qq.com>
2025-09-25 14:32:04 +0800

    rothko: set thermal

--- device.mk
+++ device.mk
@@ -180,6 +180,12 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/mediatek/libmtkperf_client \
     hardware/xiaomi
 
+# Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal-service.mediatek
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/thermal/thermal_info_config.json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config.json
 
 # Rootdir
 PRODUCT_PACKAGES += \
